e15e89840ffd3d7231ba9f87587a1825ead0c392
Benedict <benedictumeozor@gmail.com>
2025-02-23 12:04:28 +0100

    enhance logout functionality to clear queries before session removal

--- src/hooks/use-logout.ts
+++ src/hooks/use-logout.ts
@@ -3,15 +3,19 @@ import { useAppDispatch } from ".";
 import { clearSession } from "@/lib/redux/slices/session";
 import { useState } from "react";
 import { delay } from "@/utils";
+import { useQueryClient } from "@tanstack/react-query";
 
 function useLogout() {
   const [loading, setLoading] = useState(false);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const logout = async () => {
     setLoading(true);
     await delay(2000);
+    // Clear all queries first before removing the session
+    queryClient.clear();
     dispatch(clearSession());
     navigate("/login");
     setLoading(false);
